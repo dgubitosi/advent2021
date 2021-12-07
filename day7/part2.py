0d8a63a81501bf03b44859e6d90e989ea1c368b4
David Gubitosi <david.gubitosi@gmail.com>
2021-12-07 09:56:44 -0500

    day 7 solutions

---
+++ day7/part2.py
@@ -0,0 +1,24 @@
+
+max_p = 0
+positions = dict()
+with open('input.txt') as f:
+    for line in f:
+        for i in line.strip().split(','):
+            i = int(i)
+            max_p = max(max_p, i)
+            positions.setdefault(i, 0)
+            positions[i] += 1
+
+pos = 0
+fuel = -1
+for target_p in range(max_p):
+    f = 0
+    for p in positions:
+        d = abs(target_p - p)
+        sum_d = (d * (d+1)) // 2
+        f += sum_d * positions[p]
+    if fuel < 0 or f < fuel:
+        fuel = f
+        pos = target_p
+
+print(pos, fuel)
