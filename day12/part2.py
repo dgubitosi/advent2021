23b17cf4b0f22cd0532111e42083fecdd089642b
David Gubitosi <david.gubitosi@gmail.com>
2021-12-13 09:52:31 -0500

    day 12 part 2 solution

--- day12/part2.py
+++ day12/part2.py
@@ -11,13 +11,26 @@ def find_all_paths(start, end, active_path=list()):
     if start == end:
         return [active_path]
     paths = list()
-    for n in graph[start]:
-        if n not in active_path or n.isupper():
+    for n in sorted(graph[start]):
+        can_visit = False
+        if n.isupper():
+            can_visit = True
+        # n.islower()
+        else:
+            if n not in active_path:
+                can_visit = True
+            else: # n.islower()
+                if n not in active_path:
+                    can_visit = True
+                elif n not in ['start', 'end']:
+                    repeats = [ v for v in active_path if v.islower() and active_path.count(v) > 1 ]
+                    can_visit = len(repeats) == 0
+        if can_visit:
             for p in find_all_paths(n, end, active_path):
                 paths.append(p)
     return paths
 
 paths = find_all_paths('start', 'end')
 for p in paths:
-    print(p)
+    print(",".join(p))
 print(len(paths))
